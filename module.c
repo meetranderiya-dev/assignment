329046704b76eb33468fb7e2d980a98b41596006
meet <meetranderiya@gmail.com>
2026-04-21 16:58:00 +0530

    module 2

---
+++ module.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main() {
+       char ch='m'; //char => 1 byte%c
+       int X =100; // int =>4byte%d
+       float pi =3.14; //float 4 byte%f
+       printf ("\n char =%c",ch);
+       printf ("\n X =%d",X);
+       printf ("\n pi=%f",pi);
+       
+       }
