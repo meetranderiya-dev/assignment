4b624e6a8919a122562e7bc0705251d486542b6a
tops <meetranderiya@gmail.com>
2026-05-06 14:52:26 +0530

    '/c/Users/tops/Documents/meet..tops/maths total.cpp' '/c/Users/tops/Documents/meet..tops/maths total.exe' '/c/Users/tops/Documents/meet..tops/modu 2 age.cpp' /c/Users/tops/Documents/meet..tops/break.cpp /c/Users/tops/Documents/meet..tops/break.exe /c/Users/tops/Documents/meet..tops/continue.cpp /c/Users/tops/Documents/meet..tops/continue.exe '/c/Users/tops/Documents/meet..tops/else if.cpp' '/c/Users/tops/Documents/meet..tops/else if.exe' '/c/Users/tops/Documents/meet..tops/even odd.cpp' '/c/Users/tops/Documents/meet..tops/int float.cpp' '/c/Users/tops/Documents/meet..tops/int float.exe' '/c/Users/tops/Documents/meet..tops/loop do.cpp' '/c/Users/tops/Documents/meet..tops/loop do.exe' '/c/Users/tops/Documents/meet..tops/loop for.cpp' '/c/Users/tops/Documents/meet..tops/loop for.exe' '/c/Users/tops/Documents/meet..tops/loop num.cpp' '/c/Users/tops/Documents/meet..tops/loop num.exe' '/c/Users/tops/Documents/meet..tops/loop while.cpp' '/c/Users/tops/Documents/meet..tops/loop while.exe' '/c/Users/tops/Documents/meet..tops/loop x1,x2,ans.cpp' '/c/Users/tops/Documents/meet..tops/loop x1,x2,ans.exe' /c/Users/tops/Documents/meet..tops/loop.cpp /c/Users/tops/Documents/meet..tops/loop.exe

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,1 +1,12 @@
-dgdgdgdg
+#include<stdio.h>
+main(){
+	int num,i,flag=0;
+	printf("\n enter number");
+	scanf("%d",&num);
+	for(i=2;i<=num/2;i++){
+		if(num%i==0){
+			flag=1;
+		}
+	}
+	(flag==0)?printf("\n prime"):printf("\n not prime");
+}
