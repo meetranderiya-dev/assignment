18d3baf4c19217dbcec12310c61cb02cf6c1a53d
meet randeriya <meetranderiya@gmail.com>
2026-08-04 04:39:50 -0700

    Add files via upload

---
+++ s2_q1.py
@@ -0,0 +1,15 @@
+product_name = "vivo"
+price = 8000
+is_avalible = True
+rating = 5.5
+print(product_name)
+print(type(product_name))
+
+print(price)
+print(type(price))
+
+print(is_avalible)
+print(type(is_avalible))
+
+print(rating)
+print(type(rating))
